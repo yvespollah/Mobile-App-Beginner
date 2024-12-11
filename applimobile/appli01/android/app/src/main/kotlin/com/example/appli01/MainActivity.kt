aa6e6aab72a0fa7b76951db9b254ea3a2304b416
yvesprogrammeur <yvesprogrammeur@gmail.com>
2024-12-11 23:20:38 +0100

    First minimal program fron scratch for a flutter code

---
+++ applimobile/appli01/android/app/src/main/kotlin/com/example/appli01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.appli01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ applimobile/appli01/lib/main.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    // recupere n'importe quoi et affiche au centre de l'ecran
+    Center(
+      child: Text("piko prgrammeur",
+      textDirection: TextDirection.ltr,
+      ),
+    )   
+  );
+}
