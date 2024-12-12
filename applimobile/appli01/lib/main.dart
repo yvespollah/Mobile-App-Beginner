b11f0c58f5f3ad4cd8a9838cdf2cfe46230af52a
yvesprogrammeur <yvesprogrammeur@gmail.com>
2024-12-12 08:23:33 +0100

    Use of material design for widget

--- applimobile/appli01/lib/main.dart
+++ applimobile/appli01/lib/main.dart
@@ -3,18 +3,22 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(HomePage());
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
 }
 
 class HomePage extends StatelessWidget{
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context){ // la methode build a besoin d'un conret
-    return Center(
-      child: Text("piko is a boy",
-      textDirection:  TextDirection.ltr,
-      
-      ),
-    ); 
+    return Scaffold(
+      appBar: AppBar(title: const Text("pikoapp"),elevation: 12,),
+      body: const Center(child: Text("good morning yves"),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: const Icon(Icons.add),),
+    ); // il nous fournit les chose de base d'une app mobile
   }
 }                         
